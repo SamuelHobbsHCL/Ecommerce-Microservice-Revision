9e517d2d73b301c1691bd87cc2cc023db1c18830
Christopher Craze <46430832+Gek0r@users.noreply.github.com>
2022-09-12 11:14:44 -0500

    Update environment.ts

--- EcommerceUI/src/environments/environment.ts
+++ EcommerceUI/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   stripePublishableKey: "pk_test_51LfG2CDyv2YM2ORIc44V4mFeS98IC25SbMO4x3Mxjo1fzSda1hQhitfKIXBN3yhyHTAQfrHBiVaHhtwOOZuLl91Z00goFJBbcb",
-  apiUrl: "http://localhost:8080"
+  apiUrl: "https://hcl-capstone-ecommerce-group1-frontend.azurewebsites.net"
 };
 
 /*
